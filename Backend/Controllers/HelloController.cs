aefb5a8618fa878dee70a0979d5b1488e484accb
Marcel <marcelpodsiadlo.business@gmail.com>
2024-11-11 22:07:00 +0100

    Checking controller in backend Createing defaut angular app

---
+++ Backend/Controllers/HelloController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HelloController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Witaj w moim API!");
+    }
+}
