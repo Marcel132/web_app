46512c6351e72899a9a276723b8728c57b8b9b0c
Marcel <marcelpodsiadlo.business@gmail.com>
2024-12-19 23:01:25 +0100

    New Endpoint: find product by {id{

--- Backend/Controllers/FoodCaloriesController.cs
+++ Backend/Controllers/FoodCaloriesController.cs
@@ -24,4 +24,17 @@ public class FoodCaloriesController : ControllerBase
     }
     return Ok(products);
   }
+
+  [HttpGet("{id}")]
+  public async Task<IActionResult> GetProductById( string id)
+  {
+    var product = await _foodService.GetProductByIdAsync(id);
+
+    if( product == null )
+    {
+      return NotFound("No products found");
+    }
+
+      return Ok(product);
+  }
 }
--- Backend/Services/FoodService.cs
+++ Backend/Services/FoodService.cs
@@ -1,6 +1,7 @@
 using MongoDB.Driver;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using MongoDB.Bson;
 
 public class FoodService 
 {
@@ -14,5 +15,10 @@ public class FoodService
   public async Task<List<FoodModel>> GetAllProductsAsync()
   {
     return await _products.Find(product => true).ToListAsync();  
+  }
+
+  public async Task<FoodModel?> GetProductByIdAsync(string id)
+  { 
+    return await _products.Find(product => product.Id_prod == Int32.Parse(id)).FirstOrDefaultAsync();
   }
 }
