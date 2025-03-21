30143f74d5ca1ebc12df205319cfe027b1d0678e
Marcel <marcelpodsiadlo.business@gmail.com>
2025-03-21 21:45:15 +0100

    Save in variable a meal

--- Frontend/app/src/components/features/dashboard/content-dashboard/meals/meals.component.ts
+++ Frontend/app/src/components/features/dashboard/content-dashboard/meals/meals.component.ts
@@ -3,6 +3,20 @@ import { Component } from '@angular/core';
 import { Product, UserService } from '../../../../../services/user.service';
 import { FormsModule } from '@angular/forms';
 
+export interface MealsTable {
+	id_prod: number,
+	name: string,
+	weight: number,
+	details: {
+		kcal: number,
+		proteins: number,
+		fats: number,
+		carbohydrates: number,
+	}
+	showDescription?: boolean
+	editMode?: boolean
+}
+
 @Component({
   selector: 'app-meals',
   standalone: true,
@@ -17,9 +31,17 @@ export class MealsComponent {
 
 	products: Product[] = []
 	meals: any[] = []
-	selectedProductId: number | null = null
+	selectedProductById: number | null = null
 	selectedProduct: Product | null = null
 	showAddMealCard: boolean = false
+	productWeight: number = 0;
+	updateProductWeight: number = 0;
+
+	title: string = ''
+	description: string = ''
+
+	mealsTable: MealsTable[] = []
+
 
 	constructor(
 		private userService: UserService,
@@ -49,6 +71,51 @@ export class MealsComponent {
 	}
 
 	onProductChange(){
-		this.selectedProduct = this.products.find(prod => prod.id_prod == this.selectedProductId) || null;
+		this.selectedProduct = this.products.find(prod => prod.id_prod == this.selectedProductById) || null;
+	}
+	addMealToTable(){
+		if(this.selectedProduct == null || this.productWeight == 0){
+			return
+		}
+		this.mealsTable.push({
+			id_prod: this.selectedProduct.id_prod,
+			name: this.selectedProduct.name,
+			weight: this.productWeight,
+			details: {
+				kcal: this.selectedProduct.details.kcal * this.productWeight / 100,
+				proteins: this.selectedProduct.details.proteins * this.productWeight / 100,
+				fats: this.selectedProduct.details.fats * this.productWeight / 100,
+				carbohydrates: this.selectedProduct.details.carbohydrates * this.productWeight / 100,
+			},
+			showDescription: false,
+			editMode: false
+		})
+		console.log(this.mealsTable.length)
+		console.log(this.mealsTable)
+	}
+
+	toggleDescription(index: number): void {
+		this.mealsTable[index].showDescription = !this.mealsTable[index].showDescription
+	}
+	deleteMealFromTable(index: number): void {
+		this.mealsTable.splice(index, 1)
+	}
+	editMealFromTable(index: number): void {
+		this.toggleEditMode(index)
+		this.mealsTable[index].weight = this.updateProductWeight
+		if (this.selectedProduct) {
+			this.mealsTable[index].details = {
+				kcal: this.selectedProduct.details.kcal * this.updateProductWeight / 100,
+				proteins: this.selectedProduct.details.proteins * this.updateProductWeight / 100,
+				fats: this.selectedProduct.details.fats * this.updateProductWeight / 100,
+				carbohydrates: this.selectedProduct.details.carbohydrates * this.updateProductWeight / 100,
+			}
+		}
+		console.log("Index: " +  JSON.stringify(this.mealsTable[index]))
+		console.log("Tablica: " + JSON.stringify(this.mealsTable))
+	}
+
+	toggleEditMode(index: number): void {
+		this.mealsTable[index].editMode = !this.mealsTable[index].editMode
 	}
 }
