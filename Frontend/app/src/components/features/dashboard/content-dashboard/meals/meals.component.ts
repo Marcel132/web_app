6154787be40f770c85a1f8d4f12ef7234a7d7440
Marcel <marcelpodsiadlo.business@gmail.com>
2025-03-29 18:11:25 +0100

    Add comments Show user his meal

--- Frontend/app/src/components/features/dashboard/content-dashboard/meals/meals.component.ts
+++ Frontend/app/src/components/features/dashboard/content-dashboard/meals/meals.component.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Product, UserService } from '../../../../../services/user.service';
+import { UserService } from '../../../../../services/user.service';
 import { FormsModule } from '@angular/forms';
 import { MealsTable } from '../../../../../interfaces/meals-table';
 import { StateService } from '../../../../../services/state.service';
+import { Product } from '../../../../../interfaces/product';
+import { Meals } from '../../../../../interfaces/meals';
 
 @Component({
   selector: 'app-meals',
@@ -17,23 +19,26 @@ import { StateService } from '../../../../../services/state.service';
 })
 export class MealsComponent {
 
-	products: Product[] = []
-	meals: any[] = []
-	selectedProductById: number | null = null
-	selectedProduct: Product | null = null
-	showAddMealCard: boolean = false
-	productWeight: number = 0;
-	updateProductWeight: number = 0;
+	products: Product[] = [] // Product that user can select
+	selectedProductById: number | null = null // Selected product sorting by ID
+	selectedProduct: Product | null = null // Selected product details
+	showAddMealCard: boolean = false // Card is visible for user when click a button
+	visibleIndex!: number | null
+	showDetails!: boolean[]
+	weight: number = 0; // Select weight
+	editWeight: number = 0; // Edit weight
 
-	title: string = ''
-	description: string = ''
+	title: string = '' // Title of meal
+	description: string = '' // Description of meal
 
-	mealsTable: MealsTable[] = []
+	mealsTable: MealsTable[] = [] // Array with all user products
 
 	handler = {
 		state: false,
 		message: ""
-	}
+	} // Handler for server response
+
+	userMeals: Meals | null = null // User data with all meals
 
 	constructor(
 		private userService: UserService,
@@ -46,6 +51,14 @@ export class MealsComponent {
 				this.userService.fetchProductsData()
 			} else {
 				this.products = products
+			}
+		})
+		this.stateService.userMealsSubject$.subscribe(meals => {
+			if(meals == null){
+				this.userService.fetchUserMealsData()
+			} else {
+				this.userMeals = meals;
+				console.log("userMeals:", this.userMeals);
 			}
 		})
 	}
@@ -65,24 +78,27 @@ export class MealsComponent {
 		this.selectedProduct = this.products.find(prod => prod.id_prod == this.selectedProductById) || null;
 	}
 	addMealToTable(){
-		if(this.selectedProduct == null || this.productWeight == 0){
+		if(this.selectedProduct == null || this.weight == 0){
 			return
 		}
 		this.mealsTable.push({
 			id_prod: this.selectedProduct.id_prod,
 			name: this.selectedProduct.name,
-			weight: this.productWeight,
+			weight: this.weight,
 			productDetails: {
-				kcal: this.selectedProduct.productDetails.kcal * this.productWeight / 100,
-				proteins: this.selectedProduct.productDetails.proteins * this.productWeight / 100,
-				fats: this.selectedProduct.productDetails.fats * this.productWeight / 100,
-				carbohydrates: this.selectedProduct.productDetails.carbohydrates * this.productWeight / 100,
+				kcal: this.selectedProduct.productDetails.kcal * this.weight / 100,
+				proteins: this.selectedProduct.productDetails.proteins * this.weight / 100,
+				fats: this.selectedProduct.productDetails.fats * this.weight / 100,
+				carbohydrates: this.selectedProduct.productDetails.carbohydrates * this.weight / 100,
 			},
 			showDescription: false,
 			editMode: false
 		})
 	}
 
+	toggleDetails(index: number): void {
+		this.visibleIndex = this.visibleIndex === index ? null : index
+	}
 	toggleDescription(index: number): void {
 		this.mealsTable[index].showDescription = !this.mealsTable[index].showDescription
 	}
@@ -91,13 +107,13 @@ export class MealsComponent {
 	}
 	editMealFromTable(index: number): void {
 		this.toggleEditMode(index)
-		this.mealsTable[index].weight = this.updateProductWeight
+		this.mealsTable[index].weight = this.editWeight
 		if (this.selectedProduct) {
 			this.mealsTable[index].productDetails = {
-				kcal: this.selectedProduct.productDetails.kcal * this.updateProductWeight / 100,
-				proteins: this.selectedProduct.productDetails.proteins * this.updateProductWeight / 100,
-				fats: this.selectedProduct.productDetails.fats * this.updateProductWeight / 100,
-				carbohydrates: this.selectedProduct.productDetails.carbohydrates * this.updateProductWeight / 100,
+				kcal: this.selectedProduct.productDetails.kcal * this.editWeight / 100,
+				proteins: this.selectedProduct.productDetails.proteins * this.editWeight / 100,
+				fats: this.selectedProduct.productDetails.fats * this.editWeight / 100,
+				carbohydrates: this.selectedProduct.productDetails.carbohydrates * this.editWeight / 100,
 			}
 		}
 	}
@@ -107,12 +123,27 @@ export class MealsComponent {
 	}
 
 	saveMeal(){
-		this.userService.saveUserMeal(this.title, this.description, this.mealsTable)
-		.then(response => {
-			if(response) {
-				this.handler.state = response.state
-				this.handler.message = response.message
+		if(this.title.length > 80 || this.title.length == 0){
+			this.handler.state = true;
+			this.handler.message = "Twój tytuł nie spełnia wymagań"
+		}
+		else if( this.description.length > 250 || this.description.length == 0){
+			this.handler.state = true;
+			this.handler.message = "Twój opis nie spełnia wymagań"
+		}
+		else {
+			if(this.mealsTable.length > 0 && this.weight > 0){
+				this.userService.saveUserMeal(this.title, this.description, this.mealsTable)
+				.then(response => {
+					if(response) {
+						this.handler.state = response.state
+						this.handler.message = response.message
+					}
+				})
+			} else {
+				this.handler.state = true
+				this.handler.message = "Aby dodać posiłek podaj produkt i jego wagę"
 			}
-		})
+		}
 	}
 }
