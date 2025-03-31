e4498b62450a34bdb487609f7d1480f205a714ad
Marcel <marcelpodsiadlo.business@gmail.com>
2025-03-31 19:15:21 +0200

    Add graph from chart.js Create default chart with sum of all user meals

---
+++ Frontend/app/src/components/features/dashboard/content-dashboard/graph/calories-chart/calories-chart.component.ts
@@ -0,0 +1,104 @@
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import Chart from 'chart.js/auto'
+
+@Component({
+  selector: 'app-calories-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './calories-chart.component.html',
+  styleUrl: './calories-chart.component.scss'
+})
+export class CaloriesChartComponent  implements AfterViewInit{
+	@ViewChild('caloriesChart') chartRef! : ElementRef
+	@Input() dailyCalorieNeeds!: number
+	@Input() mealData!: {
+		date: string,
+		totalCalories: number,
+		totalProteins: number,
+		totalCarbohydrates: number,
+		totalFats: number
+	}[]
+
+	ngAfterViewInit() {
+		if (this.mealData && this.mealData.length > 0 && this.chartRef?.nativeElement) {
+			this.createChart();
+		} else {
+			console.error('Brak danych lub elementu canvas!');
+		}
+	}
+
+	private createChart() {
+    const ctx = this.chartRef.nativeElement.getContext('2d');
+    const labels = this.mealData.map(meal => meal.date);
+    const data = this.mealData.map(meal => meal.totalCalories);
+
+		const fats = this.mealData.map(detail => detail.totalFats)
+		const proteins = this.mealData.map(detail => detail.totalProteins)
+		const carbohydrates = this.mealData.map(detail => detail.totalCarbohydrates)
+
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: 'Spożyte kalorie',
+            data: data,
+            borderColor: 'blue',
+            backgroundColor: 'rgba(173, 216, 230, 0.4)',
+            fill: true,
+            tension: 0.4,
+            pointRadius: 5,
+          },
+          {
+            label: 'Dzienne zapotrzebowanie',
+            data: new Array(labels.length).fill(this.dailyCalorieNeeds),
+            borderColor: 'red',
+            borderWidth: 2,
+            borderDash: [5, 5],
+            fill: false,
+          },
+					{
+						label: 'Białka',
+						data: proteins,
+						borderColor: 'orange',
+						backgroundColor: 'rgba(255, 165, 0, 0.4)',
+						fill: true,
+						tension: 0.4,
+						pointRadius: 5,
+					},
+					{
+						label: 'Tłuszcze',
+						data: fats,
+						borderColor: 'green',
+						backgroundColor: 'rgba(255, 165, 0, 0.4)',
+						fill: true,
+						tension: 0.4,
+						pointRadius: 5,
+					},
+					{
+						label: 'Węgle',
+						data: carbohydrates,
+						borderColor: 'brown',
+						backgroundColor: 'rgba(255, 165, 0, 0.4)',
+						fill: true,
+						tension: 0.4,
+						pointRadius: 5,
+					},
+        ]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          tooltip: {
+            callbacks: {
+              label: (tooltipItem: any) => {
+                return `${tooltipItem.dataset.label}: ${tooltipItem.raw.toFixed(2)} g`;
+              }
+            }
+          }
+        }
+      }
+    });
+  }
+}
--- Frontend/app/src/components/features/dashboard/content-dashboard/graph/graph.component.ts
+++ Frontend/app/src/components/features/dashboard/content-dashboard/graph/graph.component.ts
@@ -1,12 +1,114 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StateService } from '../../../../../services/state.service';
+import { UserService } from '../../../../../services/user.service';
+import { Meals } from '../../../../../interfaces/meals';
+import { CaloriesChartComponent } from "./calories-chart/calories-chart.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-graph',
   standalone: true,
-  imports: [],
+  imports: [
+		CaloriesChartComponent,
+		CommonModule,
+	],
   templateUrl: './graph.component.html',
   styleUrl: './graph.component.scss'
 })
-export class GraphComponent {
+export class GraphComponent implements OnInit {
+	meals: Meals | null = null
+	transformedMealsData: any[] = []
+	dailyCalorieNeeds = 2200
 
+	constructor(
+		private stateService: StateService,
+		private userService: UserService,
+	) {}
+
+
+	ngOnInit(): void {
+		this.stateService.userMealsSubject$.subscribe({
+			next: meals => {
+				if(meals == null){
+					this.userService.fetchUserMealsData();
+				} else {
+					this.meals = meals
+					this.transformMealData()
+				}
+			},
+			error: err => {
+				console.log('Error fetching meals: ', err)
+			}
+		})
+	}
+
+	private transformMealData() {
+    if (!this.meals || !Array.isArray(this.meals.details)) return;
+
+    const dailyCalories: { [key: string]: number } = {};
+		const dailyFats: { [key: string] : number} = {}
+		const dailyProteins: { [key: string] : number} = {}
+		const dailyCarbohydrates: { [key: string] : number} = {}
+
+
+    this.meals.details.forEach(detail => {
+
+			// let totalFats: number = 0
+			// let totalCarbohydrates: number = 0
+			// let totalProteins: number = 0
+
+      const date = new Date(detail.date).toISOString().split('T')[0];
+			if (!Array.isArray(detail.meals)) return;
+      const totalCalories = detail.meals.reduce((sum, meal) => sum + meal.productDetails.kcal, 0);
+			const totalProteins = detail.meals.reduce((sum, meal) => sum + meal.productDetails.proteins, 0)
+			const totalFats = detail.meals.reduce((sum, meal) => sum + meal.productDetails.fats, 0)
+			const totalCarbohydrates = detail.meals.reduce((sum, meal) => sum + meal.productDetails.carbohydrates, 0)
+
+
+			// detail.meals.map(details => {
+			// 	totalFats += details.productDetails.fats
+			// })
+			// detail.meals.map(details => {
+			// 	totalProteins += details.productDetails.proteins
+			// })
+			// detail.meals.map(details => {
+			// 	totalCarbohydrates += details.productDetails.carbohydrates
+			// })
+
+      if (!dailyCalories[date])
+			{
+				dailyCalories[date] = 0;
+      }
+      dailyCalories[date] += totalCalories;
+
+			if(!dailyFats[date])
+			{
+				dailyFats[date] = 0
+			}
+			dailyFats[date] += totalFats
+
+			if(!dailyProteins[date])
+				{
+					dailyProteins[date] = 0
+				}
+				dailyProteins[date] += totalProteins
+
+			if(!dailyCarbohydrates[date])
+				{
+					dailyCarbohydrates[date] = 0
+				}
+				dailyCarbohydrates[date] += totalCarbohydrates
+
+
+    });
+
+    this.transformedMealsData = Object.keys(dailyCalories).map(date => ({
+      date,
+      totalCalories: dailyCalories[date],
+			totalFats: dailyFats[date],
+			totalProteins: dailyProteins[date],
+			totalCarbohydrates: dailyCarbohydrates[date],
+    }));
+		console.log(this.transformedMealsData)
+  }
 }
