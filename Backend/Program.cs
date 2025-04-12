9e68173a1b6c2e00fa1722a7c0ca2779d28117b1
Marcel <marcelpodsiadlo.business@gmail.com>
2025-04-12 20:41:46 +0200

    Add Authorize method

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -2,9 +2,14 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using DotNetEnv;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
-var ipKey = Environment.GetEnvironmentVariable("IP_DEVICE_KEY_FRONTEND") ?? throw new ArgumentNullException("IP_DEVICE_KEY_FRONTEND is missing in environment variables");
+Env.Load(); // Load environment variables from .env file
+var ipKey = Environment.GetEnvironmentVariable("IP_DEVICE_KEY_FRONTEND") 
+        ?? throw new ArgumentNullException("IP_DEVICE_KEY_FRONTEND is missing in environment variables");
+
 builder.Services.AddCors(options => 
 {
   options.AddPolicy("AllowFrontend", policy => 
@@ -19,33 +24,25 @@ builder.WebHost.ConfigureKestrel(serverOptions =>
 {
     serverOptions.ListenAnyIP(5000); // HTTP
 });
-// builder.Services.AddAuthentication("Bearer")
-//     .AddJwtBearer("Bearer", options =>
-//     {
-//         options.TokenValidationParameters = new TokenValidationParameters
-//         {
-//             ValidateIssuer = true,
-//             ValidateAudience = true,
-//             ValidateLifetime = true,
-//             ValidIssuer = "https://yourissuer.com",
-//             ValidAudience = "https://youraudience.com",
-//             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("your-secret-key"))
-//         };
-//         options.Events = new JwtBearerEvents
-//         {
-//           OnAuthenticationFailed = context => 
-//           {
-//             context.Response.StatusCode = 401;
-//             context.Response.ContentType = "application/json";
-//             return context.Response.WriteAsync(new 
-//             {
-//               message = "Authentication failed",
-//               details = context.Exception.Message
-//             }.ToString());
-//           }
-//         };
 
-//     });
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+  .AddJwtBearer(options => 
+  {
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+
+        RoleClaimType = ClaimTypes.Role,
+    };
+  });
+
+builder.Services.AddAuthorization();
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
 builder.Services.AddSingleton(sp => sp.GetRequiredService<IOptions<JwtSettings>>().Value);
 
@@ -61,8 +58,10 @@ var app = builder.Build();
 
 // app.UseAuthorization();
 // app.UseAuthentication();
-app.UseCors("AllowFrontend");
 app.UseRouting();
+app.UseCors("AllowFrontend");
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
