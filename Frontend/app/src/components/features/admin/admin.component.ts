8661e557e69d49e1d1dbecec98347598b534a34b
Marcel <marcelpodsiadlo.business@gmail.com>
2025-04-13 12:53:26 +0200

    Add navigation to components

--- Frontend/app/src/components/features/admin/admin.component.ts
+++ Frontend/app/src/components/features/admin/admin.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { StateService } from '../../../services/state.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [
+		CommonModule,
+		RouterModule
+	],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
 export class AdminComponent {
 
+	// This component is used to manage the admin panel of the application.
+	userRole: string | null = null;
+	constructor(
+		private stateService: StateService
+	) { }
+
+	ngOnInit(): void {
+		this.stateService.userRoleSubject$.subscribe((role) => {
+			this.userRole = role;
+		})
+	}
 }
