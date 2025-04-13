43cf897cb12687616607705a8bf1ba9fe5ca48bb
Marcel <marcelpodsiadlo.business@gmail.com>
2025-04-13 12:52:48 +0200

    Add new components

---
+++ Frontend/app/src/components/features/admin/users-panel/users-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './users-panel.component.html',
+  styleUrl: './users-panel.component.scss'
+})
+export class UsersPanelComponent {
+
+}
