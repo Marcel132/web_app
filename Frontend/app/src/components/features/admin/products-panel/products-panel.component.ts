34387aa39e2f7e6eba40b8ca65e3f448d49943ec
Marcel <marcelpodsiadlo.business@gmail.com>
2025-04-15 19:33:36 +0200

    Add styles, UI

--- Frontend/app/src/components/features/admin/products-panel/products-panel.component.ts
+++ Frontend/app/src/components/features/admin/products-panel/products-panel.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { StateService } from '../../../../services/state.service';
+import { Product } from '../../../../interfaces/product';
+import { UserService } from '../../../../services/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products-panel',
   standalone: true,
-  imports: [],
+  imports: [
+		CommonModule
+	],
   templateUrl: './products-panel.component.html',
   styleUrl: './products-panel.component.scss'
 })
 export class ProductsPanelComponent {
 
+	constructor(
+		private stateService: StateService,
+		private userService: UserService
+	) {}
+
+	products: Product[] | null = null;
+
+	async ngOnInit() {
+		//Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+		//Add 'implements OnInit' to the class.
+		this.stateService.products$.subscribe((products) => {
+			if(products == null){
+				this.userService.fetchProductsData()
+			} else {
+				this.products = products;
+			}
+		})
+
+		console.log(this.products);
+	}
 }
