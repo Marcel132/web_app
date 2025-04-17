06043cf1745d745035272b0943f9fa97d5f7ff39
Marcel <marcelpodsiadlo.business@gmail.com>
2025-04-17 20:49:43 +0200

    Update routes

--- Frontend/app/src/app/app.routes.ts
+++ Frontend/app/src/app/app.routes.ts
@@ -3,13 +3,12 @@ import { LoginComponent } from '../components/features/auth-container/login/logi
 import { RegisterComponent } from '../components/features/auth-container/register/register.component';
 import { DashboardComponent } from '../components/features/dashboard/dashboard.component';
 import { authGuard } from '../guards/auth.guard';
-import { CalculatorComponent } from '../components/features/dashboard/content-dashboard/calculator/calculator.component';
-import { AuthContainerComponent } from '../components/features/auth-container/auth-container.component';
+// import { CalculatorComponent } from '../components/features/dashboard/content-dashboard/calculator/calculator.component';
 import { ContentDashboardComponent } from '../components/features/dashboard/content-dashboard/content-dashboard.component';
 import { AccountDashboardComponent } from '../components/features/dashboard/content-dashboard/account-dashboard/account-dashboard.component';
 import { GraphComponent } from '../components/features/dashboard/content-dashboard/graph/graph.component';
-import { SettingsComponent } from '../components/features/dashboard/content-dashboard/settings/settings.component';
-import { MealsComponent } from '../components/features/dashboard/content-dashboard/meals/meals.component';
+// import { SettingsComponent } from '../components/features/dashboard/content-dashboard/settings/settings.component';
+// import { MealsComponent } from '../components/features/dashboard/content-dashboard/meals/meals.component';
 import { AdminComponent } from '../components/features/admin/admin.component';
 import { ProductsPanelComponent } from '../components/features/admin/products-panel/products-panel.component';
 import { AdminPanelComponent } from '../components/features/admin/admin-panel/admin-panel.component';
@@ -23,15 +22,13 @@ export const routes: Routes = [
 		children:
 		[
 			{path: '', component: ContentDashboardComponent, children: [
-				{path: 'forms', component: AuthContainerComponent, children: [
-					{path: 'register', component: RegisterComponent},
-					{path: 'login', component: LoginComponent},
-				]},
+				{path: 'forms/register', component: RegisterComponent},
+				{path: 'forms/login', component: LoginComponent},
 				{path: 'calculator', loadComponent: () => import('../components/features/dashboard/content-dashboard/calculator/calculator.component').then(m => m.CalculatorComponent)},
 				{path: 'meals', loadComponent: () => import('../components/features/dashboard/content-dashboard/meals/meals.component').then(m => m.MealsComponent)},
 				{path: 'graph', component: GraphComponent},
 				{path: 'account', component: AccountDashboardComponent},
-				{path: 'settings', component: SettingsComponent}
+				{path: 'settings', loadComponent: () => import('../components/features/dashboard/content-dashboard/settings/settings.component').then(m => m.SettingsComponent)}
 			]},
 		]
 	},
